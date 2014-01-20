8e66cc2bb8ef38abafd61cb8c1c6991ab7ad620d
CainFoool <cain.donaghey@live.co.uk>
2014-01-20 21:32:51 +0000

    Let's begin work.

---
+++ src/com/cainkilgore/adminify/Adminify.java
@@ -0,0 +1,13 @@
+package com.cainkilgore.adminify;
+
+public class Adminify extends JavaPlugin {
+	
+	public void onEnable() {
+		// Begin Adminify
+	}
+	
+	public void onDisable() {
+		// Stop Adminify
+	}
+
+}
