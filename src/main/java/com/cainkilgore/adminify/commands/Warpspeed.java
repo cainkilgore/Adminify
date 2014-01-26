354de52b77bdc099cbe041b5fd98c58916c87551
CainFoool <cain.donaghey@live.co.uk>
2014-01-26 23:34:17 +0000

    Better warp!

--- src/main/java/com/cainkilgore/adminify/commands/Warpspeed.java
+++ src/main/java/com/cainkilgore/adminify/commands/Warpspeed.java
@@ -5,6 +5,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import com.cainkilgore.adminify.Messages;
 import com.cainkilgore.adminify.Util;
@@ -28,6 +30,9 @@ public class Warpspeed implements CommandExecutor {
 			if(player.getFlySpeed() == 0.1F) {
 				player.setFlySpeed(1.0F);
 				player.setWalkSpeed(1.0F);
+				player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 7, 9999));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 4, 9999));
+				
 				Util.sendMessage(player, Messages.warpOn);
 				player.playSound(player.getLocation(), Sound.ENDERDRAGON_GROWL, 1, 6);
 				return true;
@@ -35,6 +40,8 @@ public class Warpspeed implements CommandExecutor {
 			
 			player.setWalkSpeed(0.2F);
 			player.setFlySpeed(0.1F);
+			player.removePotionEffect(PotionEffectType.SPEED);
+			player.removePotionEffect(PotionEffectType.JUMP);
 			Util.sendMessage(player, Messages.warpOff);
 			player.playSound(player.getLocation(), Sound.ENDERDRAGON_GROWL, 1, 0.1F);
 		}
