9937ec12953b30cdc386deddf89ff9ea099018fd
CainFoool <cain.donaghey@live.co.uk>
2014-01-26 02:30:39 +0000

    Solid!

--- src/main/java/com/cainkilgore/adminify/events/evtSnowman.java
+++ src/main/java/com/cainkilgore/adminify/events/evtSnowman.java
@@ -21,7 +21,7 @@ public class evtSnowman implements Listener {
 		if(player.getLocation().getBlock().getRelative(0, -1, 0).isLiquid()) return;
 		// if(!player.getLocation().getBlock().getType().isSolid()) return;
 		if(player.getLocation().getBlock().getRelative(0, -1, 0).getType() == Material.SNOW) return;
-		
+		if(player.getLocation().getBlock().getType().isSolid()) return;
 		if(player.getLocation().getBlock().getRelative(0, -1, 0).getType() != Material.AIR) {
 			final Location r = player.getLocation().getBlock().getLocation();
 			player.getLocation().getBlock().setType(Material.SNOW);
