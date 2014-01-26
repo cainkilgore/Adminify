8506ef134fb68ff9f652cb5a5b87ff79a3ed73ab
CainFoool <cain.donaghey@live.co.uk>
2014-01-26 02:18:31 +0000

    Added air check, and snow decay

--- src/main/java/com/cainkilgore/adminify/events/evtSnowman.java
+++ src/main/java/com/cainkilgore/adminify/events/evtSnowman.java
@@ -1,22 +1,30 @@
 package com.cainkilgore.adminify.events;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import com.cainkilgore.adminify.Adminify;
 import com.cainkilgore.adminify.Util;
 
 public class evtSnowman implements Listener {
 	
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e) {
-		Player player = e.getPlayer();
+		final Player player = e.getPlayer();
 		if(!Util.isSnowman(player)) return;
 		
-		if(!player.getLocation().getBlock().getRelative(0, -1, 0).isLiquid()) {
+		if(!player.getLocation().getBlock().getRelative(0, -1, 0).isLiquid() || !(player.getLocation().getBlock().getRelative(0, -1, 0).getType() == Material.AIR)) {
+			final Location r = player.getLocation().getBlock().getLocation();
 			player.getLocation().getBlock().setType(Material.SNOW);
+			Adminify.mainClass.getServer().getScheduler().runTaskLater(Adminify.mainClass, new Runnable() {
+				public void run() {
+					player.getWorld().getBlockAt(r).setType(Material.AIR);
+				}
+			}, 10 * 20);
 		}
 		
 	}
