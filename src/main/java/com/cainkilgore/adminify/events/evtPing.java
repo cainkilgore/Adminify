c2a30aa6b9e431d11458c423dad8d357b2c1f342
CainFoool <cain.donaghey@live.co.uk>
2014-02-20 01:02:37 +0000

    Fixed chat ping config not doing anything

--- src/main/java/com/cainkilgore/adminify/events/evtPing.java
+++ src/main/java/com/cainkilgore/adminify/events/evtPing.java
@@ -7,10 +7,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import com.cainkilgore.adminify.Adminify;
+
 public class evtPing implements Listener {
 	
 	@EventHandler
 	public void onAsyncPlayerChat(AsyncPlayerChatEvent e) {
+		if(!Adminify.mainClass.getConfig().getBoolean("settings.chat-ping")) return;
 		for(Player online : Bukkit.getServer().getOnlinePlayers()) {
 			if(e.getMessage().contains(online.getName())) {
 				online.playSound(online.getLocation(), Sound.ORB_PICKUP, 1, 1);
